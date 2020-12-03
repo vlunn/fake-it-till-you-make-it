bca08ba3464c397666e3f3f80e079d0361058eb8
Vivian Lunnikivi <vivian.lunnikivi@tuni.fi>
2020-12-03 16:45:46 +0200

    Import and test sources in smoe test React app.

--- smoke-test-lib/src/App.js
+++ smoke-test-lib/src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
+import add from './src-to-test/add'
 
 function App() {
+
+  const firstOperand = 1;
+  const secondOperand = 2;
+  const expectedResult = 3;
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div style={{marginLeft: '30vw'}}>
+          <h2>Simple test for add.js:</h2>
+          <p>
+            The sum of <strong>{firstOperand}</strong> and <strong>{secondOperand}</strong> is <strong>{ add(firstOperand, secondOperand) }</strong>
+          </p>
+          <p>
+            (Expected result: {expectedResult})
+          </p>
+        </div>
       </header>
     </div>
   );
