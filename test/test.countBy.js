3afc7d5c03551fe1774196e97e9515202767e0c6
Vivian Lunnikivi <vivian.lunnikivi@tuni.fi>
2020-12-05 15:14:32 +0200

    Test countBy.js.

--- test/test.countBy.js
+++ test/test.countBy.js
@@ -5,12 +5,20 @@ describe('test countBy.js', function() {
 
   describe('Testing normal behaviour', function() {
 
-    it('Basic happy case 1: ', function() {
-      throw new Error('Not implemented');
-    });
+    // Counts the occurence of different values for a given attibute from an iterable.
+    // Returns a new object containing the different values as a key 
+    it('Basic happy case 1: Counts active and inactive users from Array of user Objects-', function() {
+      const users = [
+        { 'user': 'barney', 'active': true },
+        { 'user': 'betty', 'active': true },
+        { 'user': 'fred', 'active': false }
+      ];
+    
+      const expectedResult = { 'true': 2, 'false': 1 }
 
-    it('Basic happy case 2: ', function() {
-      throw new Error('Not implemented');
+      const result = countBy(users, value => value.active);
+
+      chai.expect(result).to.equal(expectedResult);
     });
 
   });
