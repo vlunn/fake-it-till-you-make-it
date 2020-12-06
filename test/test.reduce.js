5a166f14daf29b2a36a03098a8a5ca37885a96b9
Vivian Lunnikivi <vivian.lunnikivi@tuni.fi>
2020-12-06 12:54:57 +0200

    Implement unit tests to reduce.js

--- test/test.reduce.js
+++ test/test.reduce.js
@@ -3,14 +3,27 @@ import reduce from '../src/reduce.js';
 
 describe('test reduce.js', function() {
 
+  const similarObjects = [{"name": "cat"}, {"name": "dog"}, {"name": "bird"}];
+
   describe('Testing normal behaviour', function() {
 
-    it('Basic happy case 1: ', function() {
-      throw new Error('Not implemented');
+    it('Basic happy case 1: Count objects in an Array', function() {
+      const expectedResult = similarObjects.length;
+
+      const result = reduce(similarObjects, (sum, animal) => sum + 1, 0);
+
+      chai.expect(result).to.equal(expectedResult);
     });
 
-    it('Basic happy case 2: ', function() {
-      throw new Error('Not implemented');
+    it('Basic happy case 2: collect values by key', function() {
+      const expectedResult = { '1': ['a', 'd', 'c'], '2': ['b'], '3': ['e'] };
+
+      const result = reduce({ 'a': 1, 'b': 2, 'e': 3, 'd': 1, 'c': 1 }, (result, value, key) => {
+        (result[value] || (result[value] = [])).push(key)
+        return result
+      }, {})
+
+      chai.expect(result).to.deep.equal(expectedResult);
     });
 
   });
